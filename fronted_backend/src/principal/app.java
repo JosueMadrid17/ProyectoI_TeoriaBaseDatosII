452b8f301b839cef98435d678c0b33d130755d46
rigobertomadrid <josue_madrid@unitec.edu>
2026-05-23 00:40:14 -0600

    Creacion de app, este es el main

---
+++ fronted_backend/src/principal/app.java
@@ -0,0 +1,9 @@
+package principal;
+import ui.vista_login;
+
+public class app{
+    public static void main(String[] args){
+        vista_login login = new vista_login();
+        login.setVisible(true);
+    }
+}
